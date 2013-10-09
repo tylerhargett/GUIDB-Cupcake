96e80fa0f16277014819d20de451eb6762a5a3c0
Christopher Kvamme <cpkvamme@gmail.com>
2013-10-08 20:04:06 -0500

    finished users csv parsing

--- DBUSERS.php
+++ DBUSERS.php
@@ -8,7 +8,7 @@
 #  Created by Tyler Hargett on 10/4/13.
 #
     #create connection - FILL IN SERVER USERNAME AND PASSWORD
-    $con = mysql_connect("Projectsite", "root", "giant");
+    $con = mysql_connect("Projectsite", "", "");
     if(!$con)
     {
         die('Could not connect: ' . mysql_error());
@@ -18,34 +18,32 @@
     
     
     $file_handle = fopen("A6/data/CustomCupcakesDBData-Users.csv", "r");
-
     $count = 0;
+    
     while (!feof($file_handle))
     {   
-
         $line_of_text = fgetcsv($file_handle, 4048);
         //$parts = explode(",", $line_of_text);
-        $UserId = (int)$line_of_text[0];
+        $UserId = $line_of_text[0];
         $OnMailingList = ($line_of_text[1] == "yes");
         $GivenName = $line_of_text[2];
         $Surname= $line_of_text[3];
         $StreetAddress= $line_of_text[4];
         $City= $line_of_text[5];
         $State= $line_of_text[6];
-        $ZipCode= (int)$line_of_text[7];
+        $ZipCode= $line_of_text[7];
         $EmailAddress= $line_of_text[8];
         $Password= $line_of_text[9];
         $TelephoneNumber= $line_of_text[10];
         if ($count != 0){
-            echo $count;
-        $query = "INSERT INTO Customers(custID, onMailingList, fName, lName, address, city, state, zipcode, email, password, telNumber) VALUES('$UserId','$OnMailingList','$GivenName','$Surname','$StreetAddress','$City','$State','$ZipCode','$EmailAddress','$Password','$TelephoneNumber')";
-        mysql_query($query);
+            $query = "INSERT INTO Customers(custID, onMailingList, fName, lName, address, city, state, zipcode, email, password, telNumer) VALUES('$UserId','$OnMailingList','$GivenName','$Surname','$StreetAddress','$City','$State','$ZipCode','$EmailAddress','$Password','$TelephoneNumber')";
+            mysql_query($query);
         }
-        $count = $count + 1;
-
+    echo mysql_error($con);
+    $count = $count +1;
 
     }
-        mysql_close($con);
-    echo "worked";
+    mysql_close($con);
+    echo " END";
     
 ?>
